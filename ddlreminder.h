a437f63bd539ba1ee4e033c5488347e20ccdcd53
yiyang zhang <3621048471@qq.com>
2025-05-25 22:59:45 +0800

    Update ddlreminder.h
    增加了新功能，实现了ddl添加、删除、以及保存到文件中的功能

--- ddlreminder.h
+++ ddlreminder.h
@@ -2,6 +2,16 @@
 #define DDLREMINDER_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+#include<QList>
+#include<QDate>
+#include "ddlitem.h"
+#include"ddlwidget.h"
+
+class QVBoxLayout;
+class QScrollArea;
+class ddlWidget;
 
 namespace Ui {
 class ddlReminder;
@@ -13,10 +23,37 @@ class ddlReminder : public QWidget
 
 public:
     explicit ddlReminder(QWidget *parent = nullptr);
+    void clearAll();
+    int count()const;
+    void saveToFile(const QString &filename);
+    void loadFromFile(const QString &filename);
+    void add(const QString&title,const QDate&deadline,const QString&description="");
     ~ddlReminder();
+    const QList<ddlItem>& getAllItems() const { return m_ddlItems; }
 
+public slots:
+    void refreshDisplay();
+private slots:
+    void on_pushButton_clicked();
+
+    void removeExpiredDDLs();
+    void onDDLClicked(ddlWidget *widget);
+    void onDeleteDDL(ddlWidget *widget);
+    void autoSave();
+signals:
+    void dataChanged();
 private:
     Ui::ddlReminder *ui;
+    void sortDDLs();
+    void setupScrollArea();
+    void showDescriptionDialog(const QString &title, const QString &description);
+    QList<ddlItem> m_ddlItems;
+    QScrollArea *m_scrollArea;
+    QWidget *m_scrollContent;
+    QVBoxLayout *m_mainLayout;
+    QTimer *m_expiryTimer;
+    QString m_saveFilePath = "ddl_reminder.json";
+
 };
 
 #endif // DDLREMINDER_H
